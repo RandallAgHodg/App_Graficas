97b22da2dc9d4a7d91cd6a926e549288c7f3bbeb
Randall Hodgson <randallaghodg@gmail.com>
2022-05-29 13:25:20 -0600

    Uploading some js stuff and admin login

--- js/app.js
+++ js/app.js
@@ -4,7 +4,65 @@ const contents = document.querySelectorAll(".content");
 const nav = document.querySelector(".links-container");
 const burger = document.querySelector(".burger");
 const links = document.querySelectorAll(".nav-links a");
+const formOpenBtn = document.querySelector(".hero-section-content img");
+const formCloseBtn = document.querySelector(".close-support-form");
+const formCameraBtn = document.querySelector("#camera-btn");
+const video = document.querySelector("#video");
+const canvas = document.querySelector("#video-canvas");
+const context = canvas.getContext("2d");
+const formRemember = document.querySelector("#rememberForm");
+const supportForm = document.querySelector(".chat-form");
 
+class UI {
+  enableWebCam() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      console.log("nyaa");
+      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        video.srcObj = stream;
+        video.play();
+      });
+    }
+  }
+
+  printAlert(type, message) {
+    const messageContainer = document.createElement("h1");
+    messageContainer.classList.add("alert");
+
+    if (type === "error") messageContainer.style.backgroundColor = "crimson";
+    else messageContainer.style.backgroundColor = "green";
+
+    messageContainer.textContent = message;
+    formRemember.appendChild(messageContainer);
+
+    setTimeout(() => {
+      messageContainer.remove();
+    }, 3000);
+  }
+}
+
+const uiControl = new UI();
+
+formCameraBtn.addEventListener("click", () => {
+  uiControl.enableWebCam();
+});
+formRemember?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  uiControl.printAlert(
+    "success",
+    "Se han enviado los pasos correspondientes a la recuperación de cuenta a su correo "
+  );
+});
+
+formOpenBtn.addEventListener("click", () => {
+  supportForm.style.display = "block";
+  formOpenBtn.style.display = "none";
+});
+
+formCloseBtn.addEventListener("click", () => {
+  supportForm.style.display = "none";
+  formOpenBtn.style.display = "block";
+});
 document.addEventListener("click", (e) => {
   console.log(e.target);
 });
@@ -23,7 +81,6 @@ links.forEach((link) => {
 });
 
 tabs.onclick = (e) => {
-  console.log("nyaaa");
   const id = e.target.dataset.id;
   if (id) {
     tabButton.forEach((btn) => {
