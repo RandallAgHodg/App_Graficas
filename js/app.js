a05e51363acfad6cbd60cf537f7158b00a847751
Randall Hodgson <randallaghodg@gmail.com>
2022-05-30 13:44:26 -0600

    finishing clients side js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,186 @@
-const tabs = document.querySelector(".wrapper");
-const tabButton = document.querySelectorAll(".tab-button");
-const contents = document.querySelectorAll(".content");
-const nav = document.querySelector(".links-container");
-const burger = document.querySelector(".burger");
-const links = document.querySelectorAll(".nav-links a");
-const formOpenBtn = document.querySelector(".hero-section-content img");
-const formCloseBtn = document.querySelector(".close-support-form");
-const formCameraBtn = document.querySelector("#camera-btn");
-const video = document.querySelector("#video");
-const canvas = document.querySelector("#video-canvas");
-const context = canvas.getContext("2d");
-const formRemember = document.querySelector("#rememberForm");
-const supportForm = document.querySelector(".chat-form");
+let tabs;
+let tabButton;
+let contents;
+let nav;
+let burger;
+let links;
+let formOpenBtn;
+let formCloseBtn;
+let formCameraBtn;
+let formRemember;
+let supportForm;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const uiControl = new UI();
+  if (window.location.pathname.substring(0, 6) !== "/admin") {
+    nav = document.querySelector(".links-container");
+    burger = document.querySelector(".burger");
+    links = document.querySelectorAll(".nav-links a");
+
+    burger.addEventListener("click", () => {
+      nav.classList.toggle("nav-open");
+      burger.classList.toggle("toggle");
+      burger.classList.toggle("border-none");
+    });
+
+    links.forEach((link) => {
+      link.addEventListener("click", () => {
+        nav.classList.toggle("nav-open");
+        burger.classList.toggle("toggle");
+      });
+    });
+
+    uiControl.toggleFooter();
+
+    var map = L.map("map").setView([12.12976, -86.26684], 16);
+
+    var tiles = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    ).addTo(map);
+
+    var marker = L.marker([12.12976, -86.26684]).addTo(map);
+  }
+
+  if (
+    window.location.pathname === "/" ||
+    window.location.pathname === "/houses.html" ||
+    window.location.pathname === "/apartments.html"
+  ) {
+    const houses = document.querySelectorAll(".houses-section-house-card");
+    houses.forEach((house) => {
+      house.addEventListener("click", () => {
+        location.href = "/detailHouse.html";
+      });
+    });
+  }
+
+  switch (window.location.pathname) {
+    case "/":
+      uiControl.configureBackgroundHeroImage();
+      uiControl.configureCarousel();
+      let video;
+      let canvas;
+      let context;
+      formOpenBtn = document.querySelector(".hero-section-content img");
+      formCloseBtn = document.querySelector(".close-support-form");
+      formCameraBtn = document.querySelector("#camera-btn");
+      supportForm = document.querySelector(".chat-form");
+
+      formCameraBtn.addEventListener("click", () => {
+        uiControl.enableWebCam();
+      });
+
+      formOpenBtn.addEventListener("click", () => {
+        supportForm.style.display = "block";
+        formOpenBtn.style.display = "none";
+        video = document.querySelector("#video");
+        canvas = document.querySelector("#video-canvas");
+        context = canvas.getContext("2d");
+      });
+
+      formCloseBtn.addEventListener("click", () => {
+        supportForm.style.display = "none";
+        formOpenBtn.style.display = "block";
+        uiControl.closeWebCam();
+      });
+      break;
+
+    case "/contact.html":
+      tabs = document.querySelector(".wrapper");
+      tabButton = document.querySelectorAll(".tab-button");
+      contents = document.querySelectorAll(".content");
+
+      tabs.onclick = (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+          tabButton.forEach((btn) => {
+            btn.classList.remove("active");
+          });
+          e.target.classList.add("active");
+
+          contents.forEach((content) => {
+            content.classList.remove("active");
+          });
+          const element = document.getElementById(id);
+          element.classList.add("active");
+        }
+      };
+
+      break;
+
+    case "/detailHouse.html":
+      var map = L.map("house-map").setView([12.12976, -86.26684], 16);
+
+      var tiles = L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        {
+          maxZoom: 19,
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }
+      ).addTo(map);
+
+      var marker = L.marker([12.12976, -86.26684]).addTo(map);
+
+      const videoDetail = document.querySelector(".house-video");
+      const btnPlay = document.querySelector("#pauseBtn");
+      const stopBtn = document.querySelector("#stopBtn");
+      const previousBtn = document.querySelector("#previousBtn");
+      const advanceBtn = document.querySelector("#advanceBtn");
+
+      stopBtn.addEventListener("click", () => {
+        videoDetail.pause();
+        videoDetail.currentTime = 0;
+      });
+
+      btnPlay.addEventListener("click", () => {
+        if (videoDetail.paused) videoDetail.play();
+        else videoDetail.pause();
+      });
+
+      previousBtn.addEventListener("click", () => {
+        videoDetail.currentTime -= 5;
+      });
+
+      advanceBtn.addEventListener("click", () => {
+        videoDetail.currentTime += 5;
+      });
+
+      break;
+    case "/adminRemember.html":
+      formRemember = document.querySelector("#rememberForm");
+
+      formRemember.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        uiControl.printAlert(
+          "success",
+          "Se han enviado los pasos correspondientes a la recuperación de cuenta a su correo "
+        );
+      });
+      break;
+    default:
+      break;
+  }
+});
 
 class UI {
-  enableWebCam() {
-    var cameraStream = null;
-    var mediaSupport = "mediaDevices" in navigator;
+  constructor() {
+    this.cameraStream = null;
+  }
 
-    if (mediaSupport && null == cameraStream) {
+  enableWebCam() {
+    const mediaSupport = "mediaDevices" in navigator;
+    if (mediaSupport && null === this.cameraStream) {
       navigator.mediaDevices
-        .getUserMedia({ video: true, audio: false })
-        .then(function (mediaStream) {
-          cameraStream = mediaStream;
+        .getUserMedia({ video: true, audio: true })
+        .then((mediaStream) => {
+          this.cameraStream = mediaStream;
           video.srcObject = mediaStream;
           video.play();
         })
@@ -30,8 +188,234 @@ class UI {
           console.log("unable to access camera: " + err);
         });
     } else {
-      alert("Your browser does not support media devices.");
+      alert("Su navegador no soporta el dispositivo de camara.");
       return;
+    }
+  }
+
+  closeWebCam() {
+    if (null !== this.cameraStream) {
+      this.cameraStream.getTracks().forEach((track) => {
+        track.stop();
+        video.load();
+      });
+
+      this.cameraStream = null;
+    }
+  }
+
+  configureBackgroundHeroImage() {
+    const imageHero = document.querySelector(".mask");
+
+    let i = 0;
+    let tiempo = 600;
+
+    const imagenes = ["arriba2.jpg", "arriba.jpg"];
+
+    setInterval(() => {
+      imageHero.style.backgroundPositionY = `-${tiempo}px`;
+      if (tiempo > 40) {
+        tiempo = 0;
+        imageHero.style.backgroundImage = `url(../imgs/${imagenes[i]})`;
+
+        if (i === imagenes.length - 1) {
+          i = 0;
+        } else {
+          i++;
+        }
+      } else {
+        tiempo++;
+      }
+    }, 100);
+  }
+
+  configureCarousel() {
+    var carousel = document.querySelector(".carousel");
+    var carouselContent = document.querySelector(".carousel-content");
+    var slides = document.querySelectorAll(".slide");
+    var arrayOfSlides = Array.prototype.slice.call(slides);
+    var carouselDisplaying;
+    var screenSize;
+    setScreenSize();
+    var lengthOfSlide;
+
+    function addClone() {
+      var lastSlide = carouselContent.lastElementChild.cloneNode(true);
+      lastSlide.style.left = -lengthOfSlide + "px";
+      carouselContent.insertBefore(lastSlide, carouselContent.firstChild);
+    }
+    // addClone();
+
+    function removeClone() {
+      var firstSlide = carouselContent.firstElementChild;
+      firstSlide.parentNode.removeChild(firstSlide);
+    }
+
+    function moveSlidesRight() {
+      var slides = document.querySelectorAll(".slide");
+      var slidesArray = Array.prototype.slice.call(slides);
+      var width = 0;
+
+      slidesArray.forEach(function (el, i) {
+        el.style.left = width + "px";
+        width += lengthOfSlide;
+      });
+      addClone();
+    }
+    moveSlidesRight();
+
+    function moveSlidesLeft() {
+      var slides = document.querySelectorAll(".slide");
+      var slidesArray = Array.prototype.slice.call(slides);
+      slidesArray = slidesArray.reverse();
+      var maxWidth = (slidesArray.length - 1) * lengthOfSlide;
+
+      slidesArray.forEach(function (el, i) {
+        maxWidth -= lengthOfSlide;
+        el.style.left = maxWidth + "px";
+      });
+    }
+
+    window.addEventListener("resize", setScreenSize);
+
+    function setScreenSize() {
+      if (window.innerWidth >= 1080) {
+        carouselDisplaying = 3;
+      } else {
+        carouselDisplaying = 1;
+      }
+      getScreenSize();
+    }
+
+    function getScreenSize() {
+      var slides = document.querySelectorAll(".slide");
+      var slidesArray = Array.prototype.slice.call(slides);
+      lengthOfSlide = carousel.offsetWidth / carouselDisplaying;
+      var initialWidth = -lengthOfSlide;
+      slidesArray.forEach(function (el) {
+        el.style.left = initialWidth + "px";
+        initialWidth += lengthOfSlide;
+      });
+    }
+
+    var rightNav = document.querySelector(".nav-right");
+    rightNav.addEventListener("click", moveLeft);
+
+    var moving = true;
+    function moveRight() {
+      if (moving) {
+        moving = false;
+        var lastSlide = carouselContent.lastElementChild;
+        lastSlide.parentNode.removeChild(lastSlide);
+        carouselContent.insertBefore(lastSlide, carouselContent.firstChild);
+        removeClone();
+        var firstSlide = carouselContent.firstElementChild;
+        firstSlide.addEventListener("transitionend", activateAgain);
+        moveSlidesRight();
+      }
+    }
+
+    function activateAgain() {
+      var firstSlide = carouselContent.firstElementChild;
+      moving = true;
+      firstSlide.removeEventListener("transitionend", activateAgain);
+    }
+
+    var leftNav = document.querySelector(".nav-left");
+    leftNav.addEventListener("click", moveRight);
+
+    // var moveLeftAgain = true;
+
+    function moveLeft() {
+      if (moving) {
+        moving = false;
+        removeClone();
+        var firstSlide = carouselContent.firstElementChild;
+        firstSlide.addEventListener("transitionend", replaceToEnd);
+        moveSlidesLeft();
+      }
+    }
+
+    function replaceToEnd() {
+      var firstSlide = carouselContent.firstElementChild;
+      firstSlide.parentNode.removeChild(firstSlide);
+      carouselContent.appendChild(firstSlide);
+      firstSlide.style.left = (arrayOfSlides.length - 1) * lengthOfSlide + "px";
+      addClone();
+      moving = true;
+      firstSlide.removeEventListener("transitionend", replaceToEnd);
+    }
+
+    carouselContent.addEventListener("mousedown", seeMovement);
+
+    var initialX;
+    var initialPos;
+    function seeMovement(e) {
+      initialX = e.clientX;
+      getInitialPos();
+      carouselContent.addEventListener("mousemove", slightMove);
+      document.addEventListener("mouseup", moveBasedOnMouse);
+    }
+
+    function slightMove(e) {
+      if (moving) {
+        var movingX = e.clientX;
+        var difference = initialX - movingX;
+        if (Math.abs(difference) < lengthOfSlide / 4) {
+          slightMoveSlides(difference);
+        }
+      }
+    }
+
+    function getInitialPos() {
+      var slides = document.querySelectorAll(".slide");
+      var slidesArray = Array.prototype.slice.call(slides);
+      initialPos = [];
+      slidesArray.forEach(function (el) {
+        var left = Math.floor(parseInt(el.style.left.slice(0, -2)));
+        initialPos.push(left);
+      });
+    }
+
+    function slightMoveSlides(newX) {
+      var slides = document.querySelectorAll(".slide");
+      var slidesArray = Array.prototype.slice.call(slides);
+      slidesArray.forEach(function (el, i) {
+        var oldLeft = initialPos[i];
+        el.style.left = oldLeft + newX + "px";
+      });
+    }
+
+    function moveBasedOnMouse(e) {
+      var finalX = e.clientX;
+      if (initialX - finalX > 0) {
+        moveRight();
+      } else if (initialX - finalX < 0) {
+        moveLeft();
+      }
+      document.removeEventListener("mouseup", moveBasedOnMouse);
+      carouselContent.removeEventListener("mousemove", slightMove);
+    }
+  }
+
+  toggleFooter() {
+    const btnFlotante = document.querySelector(".info-button");
+    if (btnFlotante) {
+      btnFlotante.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const footer = document.querySelector(".footer-container");
+
+        if (footer.classList.contains("activo")) {
+          footer.classList.remove("activo");
+          btnFlotante.classList.remove("activo");
+          btnFlotante.innerText = "Mas información";
+        } else {
+          footer.classList.add("activo");
+          btnFlotante.classList.add("activo");
+          btnFlotante.innerText = "X cerrar";
+        }
+      });
     }
   }
 
@@ -51,58 +435,6 @@ class UI {
   }
 }
 
-const uiControl = new UI();
-
-formCameraBtn.addEventListener("click", () => {
-  uiControl.enableWebCam();
-});
-formRemember?.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  uiControl.printAlert(
-    "success",
-    "Se han enviado los pasos correspondientes a la recuperación de cuenta a su correo "
-  );
-});
-
-formOpenBtn.addEventListener("click", () => {
-  supportForm.style.display = "block";
-  formOpenBtn.style.display = "none";
-});
-
-formCloseBtn.addEventListener("click", () => {
-  supportForm.style.display = "none";
-  formOpenBtn.style.display = "block";
-});
 document.addEventListener("click", (e) => {
   console.log(e.target);
 });
-
-burger.addEventListener("click", () => {
-  nav.classList.toggle("nav-open");
-  burger.classList.toggle("toggle");
-  burger.classList.toggle("border-none");
-});
-
-links.forEach((link) => {
-  link.addEventListener("click", () => {
-    nav.classList.toggle("nav-open");
-    burger.classList.toggle("toggle");
-  });
-});
-
-tabs.onclick = (e) => {
-  const id = e.target.dataset.id;
-  if (id) {
-    tabButton.forEach((btn) => {
-      btn.classList.remove("active");
-    });
-    e.target.classList.add("active");
-
-    contents.forEach((content) => {
-      content.classList.remove("active");
-    });
-    const element = document.getElementById(id);
-    element.classList.add("active");
-  }
-};
